2a9bb7305204620b108e46679ec9a7c9f60820b4
Adam Finn Tulinius <atu@dbc.dk>
2016-06-14 13:01:02 +0200

    log as json

--- morannon.go
+++ morannon.go
@@ -3,32 +3,43 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	log "github.com/Sirupsen/logrus"
 	"github.com/dbcdk/go-smaug/smaug"
 	"github.com/vulcand/oxy/forward"
 	"github.com/vulcand/oxy/roundrobin"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"strconv"
 )
 
 var (
-	app          = kingpin.New("timeattack", "Replays http requests").Version("1.0")
-	httpPort     = kingpin.Flag("port", "Http port to listen on").Default("8080").Int()
-	marathons    = kingpin.Flag("marathon", "url to marathon (repeatable for multiple instances of marathon)").Required().Strings()
-	forwarder, _ = forward.New()
+	app            = kingpin.New("timeattack", "Replays http requests").Version("1.0")
+	httpPort       = kingpin.Flag("port", "Http port to listen on").Default("8080").Int()
+	marathons      = kingpin.Flag("marathon", "url to Marathon (repeatable for multiple instances of marathon)").Required().Strings()
+	smaug_location = kingpin.Flag("smaug", "url to Smaug").Required().String()
+	forwarder, _   = forward.New()
 )
+
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+}
 
 func main() {
 	kingpin.Parse()
+	smaugUrl, err := url.Parse(*smaug_location)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.WithFields(log.Fields{"app": "morannon", "event": "started"}).Info("morannon started")
 
 	marathon, _ := roundrobin.New(forwarder)
 	for _, marathon_url := range *marathons {
 		u, err := url.Parse(marathon_url)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 
 		marathon.UpsertServer(u)
@@ -47,13 +58,25 @@ func main() {
 	redirect := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		token, err := smaug.TokenFromRequest(req)
 		if err != nil {
+			log.WithFields(log.Fields{
+				"app":   "morannon",
+				"event": "authentication_failed",
+				"error": err.Error(),
+			}).Info("request rejected")
+
 			http.Error(w, err.Error(), http.StatusUnauthorized)
 			return
 		}
 
-		smaugUrl := url.URL{Scheme: "http", Host: "platform-i01.dbc.dk:3001"}
-		identity, err := smaug.Authenticate(smaugUrl, token)
+		identity, err := smaug.Authenticate(*smaugUrl, token)
 		if err != nil {
+			log.WithFields(log.Fields{
+				"app":   "morannon",
+				"event": "authentication_failed",
+				"token": *token,
+				"error": err.Error(),
+			}).Info("request rejected")
+
 			http.Error(w, err.Error(), http.StatusForbidden)
 			return
 		}
@@ -87,6 +110,8 @@ func main() {
 			req.Body = ioutil.NopCloser(bytes.NewReader(newBody))
 			req.ContentLength = int64(len(newBody))
 		}
+
+		log.WithFields(log.Fields{"app": "morannon", "event": "started"}).Info("forwarding request")
 
 		marathon.ServeHTTP(w, req)
 	})
