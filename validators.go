d399465d65a8da07b080169e055038d5985b93d9
Adam Finn Tulinius <atu@dbc.dk>
2016-06-16 13:02:10 +0200

    validate job ID depth

--- validators.go
+++ validators.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"github.com/dbcdk/go-smaug/smaug"
+	"strings"
 )
 
 func validatePresenceOfHealthChecks(app App, identity smaug.Identity) error {
@@ -23,4 +24,16 @@ func validatePresenceOfEnvLabel(app App, identity smaug.Identity) error {
 	}
 
 	return errors.New("env-label must be present and equal one of dev|staging|prod")
+}
+
+func validateJobId(app App, identity smaug.Identity) error {
+	if !strings.HasPrefix(app.Id, "/") {
+		return errors.New("Job ID must start with a '/'")
+	}
+
+	if strings.Count(app.Id, "/") < 3 {
+		return errors.New("Job ID must have a depth of at last 3, e.g. /foo/bar/my-app")
+	}
+
+	return nil
 }
